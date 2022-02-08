92f452b58fab57fabc356c3ad07b442ad483e42d
Kseniia Kovelenko <kseniiakovalenko.94@gmail.com>
2022-02-08 17:06:29 +0200

    Add changes

---
+++ assets/JS/index.js
@@ -0,0 +1,14 @@
+function MyArray() {
+  this.length = 0;
+}
+
+// prototype obj
+const arrayPrototype = {
+  includes: function () {
+    return this.length
+  },
+  indexOf: function() {
+
+  }
+}
+
