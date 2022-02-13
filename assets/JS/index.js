a42a711186cba7ff1ea202d71b1dab2bfe469fc9
Kseniia Kovalenko <kseniiakovalenko.94@gmail.com>
2022-02-13 16:38:06 +0200

    Add base MyArray

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,16 +1,37 @@
 "use strict";
 
-function MyArray() {
-  this.length = 0;
-}
-
 // prototype obj
 const arrayPrototype = {
-  includes: function () {
-    return this.length
+  // Class.prototype.includes
+  includes: function (arg) {
+    for(let i = 0;i < this.length;i++) {
+      if(this.array[i] === arg) {
+        return true;
+      }
+    };
+    return false;
   },
   indexOf: function() {
 
   }
-}
+};
 
+// constructor func
+function MyArray() {
+  this.array = {};
+  this.length = 0;
+};
+
+// bind prototype obj (Class.prototype)
+MyArray.prototype = Object.assign(MyArray.prototype, arrayPrototype);
+
+// static method (Class.method)
+MyArray.isMyArray = function(arg) {
+  if (arg instanceof MyArray) {
+    return true;
+  };
+  return false;
+};
+
+// create instance
+const myarray = new MyArray();
